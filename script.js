648c109da67b2b2ad21fa046841a805230c3edae
Danilo Nasta <danilo.nasta.dn@gmail.com>
2023-04-03 11:47:46 +0200

    digital_clock_complete

---
+++ script.js
@@ -0,0 +1,28 @@
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+    
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    let time = h + ":" + m + ":" + s + " " + session;
+    document.querySelector("#clockDisplay").innerText = time;
+    document.querySelector("#clockDisplay").innerContent = time;
+    
+    setTimeout(showTime, 1000);
+}
+
+showTime();
